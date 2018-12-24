cde5adceeec2f48ef2dbc5d9593897233f9f4d9e
Sandeep Shivajirao <sndy.chvn.fr@gmail.com>
2018-12-25 00:56:29 +0100

    Create Prog_OpenCV_1.py

---
+++ Prog_OpenCV_1.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+img = cv2.imread('D:\EPITA\PRI\Samples\BusyStreet.jpg',cv2.IMREAD_GRAYSCALE)
+
+#cv2.IMREAD_GRAYSCALE , IMREAD_COLOR , IMREAD_UNCHANGED
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.plot()
+plt.show()
